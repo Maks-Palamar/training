797c716ec2c8aae78e03bb680279ced7b2907fe6
Maksym Palamarchuk <97300407+Maks-Palamar@users.noreply.github.com>
2023-12-02 18:51:34 +0200

    Initial commit

---
+++ js/task-1.js
@@ -0,0 +1,18 @@
+function makeTransaction(quantity, pricePerDroid) {
+    return `You ordered ${quantity} droids worth ${pricePerDroid} credits!`;
+};
+
+console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 5000)); // "You ordered 10 droids worth 5000 credits!"
+
+
+/*
+function makeTransaction(quantity, pricePerDroid) {
+    console.log(`You ordered ${quantity} droids worth ${pricePerDroid} credits!`);
+};
+
+makeTransaction(5, 3000);
+makeTransaction(3, 1000);
+makeTransaction(10, 5000);
+*/
